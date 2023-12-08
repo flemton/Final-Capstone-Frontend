c1b9788b8c550ce8da03024cf3acb7b5080131ce
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-08 18:36:55 -0500

    Add Sidebar component for render in Homepage

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import 'boxicons';
+import Logo from './Logo';
+import SidebarIcons from './SidebarIcons';
+import Footer from './Footer';
+
+const Sidebar = () => (
+  <>
+    <div className="d-flex flex-column justify-content-between align-content-center vh-100 py-4">
+      <div className="d-flex flex-column justify-content-between align-items-center gap-5">
+        <Logo />
+        <ul className="list-unstyled align-self-start">
+          <li>RESERVE MODEL</li>
+          <li>MY RESERVATIONS</li>
+          <li>ADD CARS</li>
+          <li>
+            <button type="button">DELETE CAR</button>
+          </li>
+        </ul>
+      </div>
+
+      <div className="">
+        <SidebarIcons />
+        <Footer />
+      </div>
+    </div>
+  </>
+);
+
+export default Sidebar;
