fedf7c9d9f4f588d0b7626ee83455c6795c07c3d
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-08 18:37:44 -0500

    Render Sidebar component for render in Homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Sidebar from '../Sidebar/Sidebar';
 
 const Home = () => (
-  <div>
-    <h1>Home</h1>
+  <div className="background-container container-fluid">
+    <div className="row">
+      <div className="col-3 col-3-md bg-white d-flex flex-column justify-content-between rigth-border side-hide">
+        <Sidebar />
+      </div>
+    </div>
   </div>
 );
 
