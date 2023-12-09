a2d9cb313f1a7b5e4f1990882e8bb31d1e772a62
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-08 19:55:53 -0500

    Add color-rosetta to CarDetails widget

--- src/components/CarDetails/CarDetails.jsx
+++ src/components/CarDetails/CarDetails.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import colorRosetta from './color-rosetta.png';
 
 const CarDetails = () => (
   <>
     <div className="container-fluid bg-white rounded-3 p-3">
-      <div className="text-end">
-        <h3>Tesla model X</h3>
-        <p>£0.00 deposit upon any Car</p>
+      <div className="text-end mt-2">
+        <h2 className="m-0">Tesla model X</h2>
+        <p className="tiny-font">£0.00 deposit upon any Car</p>
       </div>
 
-      <table className="table table-striped" style={{ fontSize: '0.8rem' }}>
+      <table className="table table-striped tiny-font mt-3 mb-2">
         <tbody>
           <tr>
             <td>Finance Fee:</td>
@@ -29,9 +30,23 @@ const CarDetails = () => (
         </tbody>
       </table>
       <p>
-        <span className="fw-bold">5.29 APR</span>
+        <span className="fw-bold m-0">5.29 APR</span>
         &nbsp;Representative
       </p>
+
+      <div className="text-center mx-3 my-4">
+        <p className="fw-bold tiny-font">
+          Discover more models&nbsp;
+          <span>&#62;</span>
+        </p>
+        <img src={colorRosetta} alt="Example" />
+      </div>
+
+      <div className="text-center my-2">
+        <button className="btn btn-dark px-5 text-white" type="button">
+          Reserve
+        </button>
+      </div>
     </div>
   </>
 );
