0610de3d48941558f270344740859bd301fec9ba
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-08 20:34:58 -0500

    Link Route paths for sidebar interactions

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import 'boxicons';
 import Logo from './Logo';
 import SidebarIcons from './SidebarIcons';
@@ -9,10 +10,16 @@ const Sidebar = () => (
     <div className="d-flex flex-column justify-content-between align-content-center vh-100 py-4 bg-white">
       <div className="d-flex flex-column justify-content-between align-items-center gap-5">
         <Logo />
-        <ul className="list-unstyled text-center px-4">
-          <li>RESERVE MODEL</li>
-          <li>MY RESERVATIONS</li>
-          <li>ADD CARS</li>
+        <ul className="sidebar-ul text-center">
+          <li>
+            <Link to="/reserve">RESERVE MODEL </Link>
+          </li>
+          <li>
+            <Link to="/user_reservations">MY RESERVATIONS</Link>
+          </li>
+          <li>
+            <Link to="/addcar">ADD CARS</Link>
+          </li>
           <li>
             <button type="button">DELETE CAR</button>
           </li>
