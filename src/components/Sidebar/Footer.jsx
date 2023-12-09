cbd39525289b80491df597b4f52424238b16d933
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-08 19:56:11 -0500

    Refactor tiny-fonts CSS class

--- src/components/Sidebar/Footer.jsx
+++ src/components/Sidebar/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Footer = () => <p className="text-center mx-4">2023 Tesla Motors Inc.</p>;
+const Footer = () => <p className="text-center mx-4 tiny-font">2023 Tesla Motors Inc.</p>;
 
 export default Footer;
