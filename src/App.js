bcff16cf86ae6f011b4602aa1d427dff543374ec
Alhassan Osman <oalhassan847@gmail.com>
2023-12-12 23:51:52 +0000

    Add AddCar and ReservationList components Routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/Home/Home';
+import AddCar from './components/AddCar/AddCar';
+import ReservationList from './pages/ReservationsList/ReservationList';
 import './App.css';
-import Login from './components/Session/Login';
-import Register from './components/Session/Register';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
         <Route path="/home" />
-        <Route path="/addcar" />
-        <Route path="/user_reservations" />
+        <Route path="/addcar" element={<AddCar />} />
+        <Route path="/user_reservations" element={<ReservationList />} />
         <Route path="/reserve" />
       </Routes>
     </Router>
