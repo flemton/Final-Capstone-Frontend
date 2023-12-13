1cc3e91dc894d015f2e46a0ea20aca0eac5aa38e
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-13 11:58:08 -0500

    Add sidebar toggle button

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,40 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import 'boxicons';
 import Logo from './Logo';
 import SidebarIcons from './SidebarIcons';
 import Footer from './Footer';
+import 'boxicons';
 
-const Sidebar = () => (
-  <>
-    <div className="sidebar d-flex flex-column justify-content-between align-content-center vh-100 py-4 bg-white">
-      <div className="d-flex flex-column justify-content-between align-items-center gap-5">
-        <Logo />
-        <ul className="sidebar-ul text-center align-self-end">
-          <li>
-            <NavLink to="/" className="sidebar-navlink" activeClassName="active">HOME </NavLink>
-          </li>
-          <li>
-            <NavLink to="/reserve" activeClassName="active">RESERVE MODEL </NavLink>
-          </li>
-          <li>
-            <NavLink to="/user_reservations" activeClassName="active">MY RESERVATIONS</NavLink>
-          </li>
-          <li>
-            <NavLink to="/addcar" activeClassName="active">ADD CARS</NavLink>
-          </li>
-          <li>
-            <button type="button">DELETE CAR</button>
-          </li>
-        </ul>
-      </div>
+const Sidebar = () => {
+  const [sidebarVisible, setSidebarVisible] = useState(true);
 
-      <div className="">
-        <SidebarIcons />
-        <Footer />
+  const handleClick = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
+
+  return (
+    <>
+      <div className="d-flex vh-100">
+        <div className={`sidebar d-flex flex-column justify-content-between align-content-center py-4 bg-white ${sidebarVisible ? 'show' : 'hide'}`}>
+          <div className="d-flex flex-column justify-content-between align-items-center gap-5">
+            <Logo />
+            <ul className="sidebar-ul text-center align-self-end">
+              <li>
+                <NavLink to="/" className="sidebar-navlink" activeClassName="active">
+                  HOME
+                  {' '}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/reserve" activeClassName="active">
+                  RESERVE MODEL
+                  {' '}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/user_reservations" activeClassName="active">
+                  MY RESERVATIONS
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/addcar" activeClassName="active">
+                  ADD CARS
+                </NavLink>
+              </li>
+              <li>
+                <button type="button">DELETE CAR</button>
+              </li>
+            </ul>
+          </div>
+
+          <div className="">
+            <SidebarIcons />
+            <Footer />
+          </div>
+        </div>
+
+        <button type="button" onClick={handleClick} className={`sidebar-btn btn btn-primary align-self-end my-3 ${sidebarVisible ? 'mx-0' : 'mx-3'}`}>
+          {sidebarVisible ? <box-icon type="solid" name="left-arrow" /> : <box-icon type="solid" name="right-arrow" />}
+        </button>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Sidebar;
