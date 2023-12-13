cf984870b44b9ee847e0877bc809e8271df479a7
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-13 16:23:37 -0500

    FIX mobile horizontal bug

--- src/components/Menu/BurgerMenu.jsx
+++ src/components/Menu/BurgerMenu.jsx
@@ -4,6 +4,7 @@ import Logo from '../Sidebar/Logo';
 import SidebarIcons from '../Sidebar/SidebarIcons';
 import Footer from '../Sidebar/Footer';
 import 'boxicons';
+import CarModel from './CarModel';
 
 const BurgerMenu = () => {
   const [sidebarVisible, setSidebarVisible] = useState(false);
@@ -14,24 +15,15 @@ const BurgerMenu = () => {
   return (
     <>
       <div className="burger-menu position-absolute top-0 w-100 d-flex flex-row-reverse justify-content-between align-items-start gap-5 z-2">
-        <div className="d-flex justify-content-between align-items-start w-100">
+        <div className="burger-menu-header d-flex justify-content-between align-items-start w-100">
           <div
-            className={`d-flex flex-column justify-content-between align-content-center pt-4 pb-3 w-100 ps-4 ${
+            className={`burger-menu-header-model d-flex flex-column justify-content-between align-content-center pt-4 pb-3 w-100 ps-4 ${
               sidebarVisible ? 'hide' : 'show'
             }`}
           >
             <Logo />
+            <CarModel />
 
-            <div className="d-flex flex-column align-items-center ps-5 mt-5">
-              <h1 className="text-center font-weight-bold mt-3">Model 3</h1>
-              <p className="text-nowrap text-muted">
-                Order Online for
-                {' '}
-                <span className="text-decoration-underline">
-                  Touchless Delivery
-                </span>
-              </p>
-            </div>
           </div>
 
           <button
---
+++ src/components/Menu/CarModel.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CarModel = () => (
+  <div className="car-model d-flex flex-column align-items-center mt-5">
+    <h1 className="text-center font-weight-bold mt-3">Model 3</h1>
+    <p className="text-wrap text-muted text-center">
+      Order Online for &nbsp;
+      <span className="text-decoration-underline">
+        Touchless Delivery
+      </span>
+    </p>
+  </div>
+);
+
+export default CarModel;
