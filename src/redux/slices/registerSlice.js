8f6ac3e668568873bbb591e3f4cb4fed4cbfb302
Alhassan Osman <oalhassan847@gmail.com>
2023-12-14 15:21:29 +0000

    Create registerSlice

---
+++ src/redux/slices/registerSlice.js
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const registerAsync = createAsyncThunk(
+  'register/registerAsync',
+  async ({ username, email, password }) => {
+    const response = await fetch('http://localhost:3000/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          email,
+          password,
+        },
+      }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }
+    const errorData = await response.json();
+    throw new Error(errorData.message);
+  },
+);
+
+const registerSlice = createSlice({
+  name: 'register',
+  initialState: {
+    data: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerAsync.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(registerAsync.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(registerAsync.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const selectRegisterData = (state) => state.register.data;
+export const selectRegisterLoading = (state) => state.register.loading;
+export const selectRegisterError = (state) => state.register.error;
+
+export default registerSlice.reducer;
