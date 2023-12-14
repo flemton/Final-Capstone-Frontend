76c7d89246f00d8788d14eb564f4802faebcfac3
Alhassan Osman <oalhassan847@gmail.com>
2023-12-14 15:21:44 +0000

    Add registerReducer to global store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import registerReducer from './slices/registerSlice';
 
 const store = configureStore({
   reducer: {
     // Here we will be adding reducers.
+    register: registerReducer,
   },
 });
 
