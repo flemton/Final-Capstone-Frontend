2800c06742c2aa0c1d2d599ebc58a58b396c892e
Alhassan Osman <oalhassan847@gmail.com>
2023-12-15 14:35:03 +0000

    Made sure user is authenticated before accessing home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import CarDetails from '../../components/CarDetails/CarDetails';
 import BurgerMenu from '../../components/Menu/BurgerMenu';
+import Session from '../../components/Session/Session';
 
-const Home = () => (
-  <div className="d-flex justify-content-between align-items-center background-container">
-    <div>
-      <Sidebar />
-      <BurgerMenu />
-    </div>
+const Home = () => {
+  const user = useSelector((state) => state.login.data);
 
-    <div className="mx-3">
-      <CarDetails />
+  return !user?.success ? (
+    <Session />
+  ) : (
+    <div className="d-flex justify-content-between align-items-center background-container">
+      <div>
+        <Sidebar />
+        <BurgerMenu />
+      </div>
+
+      <div className="mx-3">
+        <CarDetails />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
