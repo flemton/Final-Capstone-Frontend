71fc0b545125c357760620039c1cd045e67ecde1
Alhassan Osman <oalhassan847@gmail.com>
2023-12-16 01:53:09 +0000

    Add car reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import registerReducer from './slices/registerSlice';
 import loginReducer from './slices/loginSlice';
+import carReducer from './slices/carSlice';
 
 const store = configureStore({
   reducer: {
     // Here we will be adding reducers.
     register: registerReducer,
     login: loginReducer,
+    car: carReducer,
   },
 });
 
